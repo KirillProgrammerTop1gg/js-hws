18d9bf0d765130261638480a44bd81460705e873
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2023-07-27 13:22:04 +0300

    add hw-js-5

---
+++ hw-js-5/js/main.js
@@ -0,0 +1,16 @@
+const reset = 5 + 5+ '5';
+console.log(reset);
+
+const email = "kirillwork31@gmail.com";
+console.log("Email: "+email+"; Is this email includes '@'? "+String(email.includes("@"))+"; the length fo this string equal "+String(email.length)+" symbols. ");
+
+const str1 = "My";
+const str2 = "name";
+const str3 = "is";
+let fullName = str1 + ' ' + str2 + ' ' + str3;
+fullName += ' Viktor';
+console.log(fullName);
+
+const userName = "Олександр";
+const payment = 300;
+console.log("Дякуємо, "+userName+"о! До сплати "+payment+" гривень");
