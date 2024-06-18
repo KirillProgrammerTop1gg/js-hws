0dca838ecc1487f76f97f33ea7e90fac6905ff91
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2024-06-18 19:14:05 +0300

    fix hw-js-32

--- hw-js-32/src/js/main.js
+++ hw-js-32/src/js/main.js
@@ -4,7 +4,16 @@ class CountdownTimer {
         this.targetDate = timerData.targetDate;
     }
     startTimer() {
-        setInterval(() => {
+        const timerId = setInterval(() => {
+            if ((this.targetDate - new Date()) <= 0) {
+                clearInterval(timerId);
+                document.querySelector('.title').innerHTML = `Час закінчився`
+                document.querySelector(`${this.selector} span[data-value="days"]`).innerHTML = '0';
+                document.querySelector(`${this.selector} span[data-value="hours"]`).innerHTML = '0';
+                document.querySelector(`${this.selector} span[data-value="mins"]`).innerHTML = '0';
+                document.querySelector(`${this.selector} span[data-value="secs"]`).innerHTML = '0';
+                return false;
+            }
             const time = this.targetDate - new Date();
             const days = Math.floor(time / (1000 * 60 * 60 * 24));
             const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -14,14 +23,13 @@ class CountdownTimer {
             document.querySelector(`${this.selector} span[data-value="hours"]`).innerHTML = hours;
             document.querySelector(`${this.selector} span[data-value="mins"]`).innerHTML = mins;
             document.querySelector(`${this.selector} span[data-value="secs"]`).innerHTML = secs;
-            console.log(days, hours, mins, secs)
         }, 1000)
     }
 }
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 25, 2019'),
+  targetDate: new Date('June 18, 2024, 19:13'),
 });
 
 timer.startTimer();
