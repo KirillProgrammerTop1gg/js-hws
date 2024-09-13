656a8a9ee7ca47446418cec5942c8dc4c5f2656f
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2024-09-13 18:04:34 +0300

    fix hw-js-44

--- hw-js-44/src/App.js
+++ hw-js-44/src/App.js
@@ -23,17 +23,23 @@ const SubTitle = styled.h2`
 
 export default class App extends Component {
     state = {
-        contacts: [
-            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-        ],
+        contacts: [],
         filter: '',
         name: '',
         number: '',
         showUpdate: false,
         id: '',
+    }
+    componentDidMount(){
+        this.setState({ contacts: localStorage.getItem('contacts') !== null && JSON.parse(localStorage.getItem('contacts')).length !== 0 ? JSON.parse(localStorage.getItem('contacts')) : [
+            {id: 'id-1', name: 'Rosie Simpson', number: '+14591256'},
+            {id: 'id-2', name: 'Hermione Kline', number: '+14438912'},
+            {id: 'id-3', name: 'Eden Clements', number: '+16451779'},
+            {id: 'id-4', name: 'Annie Copeland', number: '+12279126'},
+        ] });
+    }
+    componentDidUpdate(){
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
     render(){
         return <>
