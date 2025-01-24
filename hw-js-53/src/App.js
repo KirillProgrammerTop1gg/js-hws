26524d0bd177be3038761308f971dc2d6111dce2
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2025-01-25 00:24:29 +0200

    fff

--- hw-js-53/src/App.js
+++ hw-js-53/src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import AddForm from './Components/AddForm/AddForm';
 import FilterInput from './Components/FilterInput/FilterInput';
-import {nanoid} from 'nanoid';
 import ContactList from './Components/ContactList/ContactList';
 
 const Title = styled.h1`
@@ -17,21 +14,6 @@ const SubTitle = styled.h2`
 `; 
 
 const App = () => {
-  // const [contacts, setContacts] = useState([]);
-  // const [name, setName] = useState('');
-  // const [number, setNumber] = useState('');
-  // const [filter, setFilter] = useState('');
-  // const setNameFunc = (val) => setName(val);
-  // const setNumberFunc = (val) => setNumber(val);
-  // const addContactFunc = (e) => e.target.checkValidity() ? contacts.find(contact => contact.name.trim().toLowerCase() === name.trim().toLowerCase()) === undefined ? setContacts([
-  //   ...contacts,
-  //   { id: nanoid(), name, number }
-  // ]) : alert(`${name} is already in contacts.`) : e.target.reportValidity();
-  // const setFilterFunc = (val) => setFilter(val);
-  // const delContact = (id) => setContacts(contacts.filter(contact => contact.id !== id))
-  // const contacts = useSelector((state) => state.contacts);
-  // const filter = useSelector((state) => state.filter);
-  // const dispatch = useDispatch();
   return (<>
     <Title>Phonebook</Title>
     <AddForm/>
